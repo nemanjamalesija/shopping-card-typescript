b113156e7cf393efbb78061caf7dc3cdfc72a591
Nemanja Malesija <malesija13@gmail.com>
2023-01-27 18:34:19 +0100

    add initial type and state, add reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, { useReducer } from 'react';
+import reducer from './reducer';
 
 import './App.css';
 
+export type APIproducts = {
+  id: string;
+  title: string;
+  price: number;
+  description?: string;
+  category: string;
+  image: string;
+  rating: {
+    rate: number;
+    count: number;
+  };
+}[];
+
+const initialState: APIproducts = [
+  {
+    id: '',
+    title: '',
+    price: 0,
+    category: '',
+    image: '',
+    rating: {
+      rate: 0,
+      count: 0,
+    },
+  },
+];
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return <div className="App"></div>;
 }
 
