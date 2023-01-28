69e751e06583ee6383f86c1b56ad5723bd5ffc1d
Nemanja Malesija <malesija13@gmail.com>
2023-01-28 17:18:04 +0100

    Add nav, implement track number of cart items

--- src/reducer.ts
+++ src/reducer.ts
@@ -5,7 +5,10 @@ const reducer = (state: APIproducts, action: ACTIONS): APIproducts => {
   if (action.type === 'SET_PRODUCTS') {
     if (action.payload === undefined) return { ...state };
 
-    return { ...state, cart: [...action.payload] };
+    return {
+      ...state,
+      cart: [...action.payload],
+    };
   }
 
   return state;
